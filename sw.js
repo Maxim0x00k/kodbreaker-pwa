1ee83dabea7a0ad752119c1e7dcb37c7f540da38
Maxim0x00k <95305923+Maxim0x00k@users.noreply.github.com>
2026-03-26 21:42:06 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'kb-cache-gh1';
+
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './kb-logo.png',
+  './icon-180.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return Promise.allSettled(
+        urlsToCache.map(url => cache.add(url).catch(err => console.log('Ошибка кэша:', url)))
+      );
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) return cachedResponse;
+      return fetch(event.request);
+    })
+  );
+});
